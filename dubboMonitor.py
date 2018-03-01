bcd3a6ebc73f4c90649aed98a86ff810fa8d637e
huihui19901993 <1451944847@qq.com>
2018-03-01 18:39:57 +0800

    Update dubboMonitor.py

--- dubboMonitor.py
+++ dubboMonitor.py
@@ -1,1 +1,28 @@
+import Queue
+import json
+import requests
+import telnetlib
+import threading
 
+def monitor_dubbo(hostname, name_port):
+  ```
+  检查dubbo的provider服务是否正常
+  ：param  hostname
+  ：param  name_port：字典格式  {'easy-roa':'29000','easy-mobile':'29100'}
+  ：return 
+  ```
+  result = {}
+  threads = {}
+  #这里的q，如果申明为全局变量，会导致不同的请求过来，结果混乱，比如162环境的数据，返回到186的结果中
+  q = Queue.Queue()
+  for name,port in name_port.items():
+    threads.append(threading.Thread(target = monitor_dubbo_service,args=(hostname, int(port),name,p)))
+  for t in threads:
+    t.start()
+  for t in threads:
+    t.join() 
+  while not q.emlty():
+    result.update(q.get)
+  return result
+
+def monitor_dubbo_service(hostname, port, name, p):

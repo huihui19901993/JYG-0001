08bbf58aa6ae7dbffb03a399003f7d498891ca0d
huihui19901993 <1451944847@qq.com>
2018-03-14 19:15:47 +0800

    Update jacocoCovReport.py

--- jacocoCovReport.py
+++ jacocoCovReport.py
@@ -42,6 +42,50 @@ class ModuleInfo(db.Model):
   dubbo_monitor = db.Column(db.Boolean, default = False)
   cov_monitor = db.Column(db.Boolean, default = False)
   pin_monitor = db.Column(db.Boolean, default = False)
+    
+#------------3-2、CovReport-----------#
+class CovReport(db.Model):
+    __tablename__ = 'covreport'
+    query_class = CovReportQuery
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    host = db.Column(db.String(64))
+    name = db.Column(db.String(64))
+    valid = db.Column(db.Integer,default = 1)
+    
+#-------------------------------------#
 #------查询结果不同格式返回-------#  
 class ModuleInfoQuery(BaseQuery):
+    def getHostCovModules(self, host):
+        result = self.filter_by(host=host,valid=1,cov_monitor=1).all()
+        return result
+        
 #-----------相关数据库操作-------#
+
+#---------4、生成dump文件且产生报告-----------#
+#-----------dump_data-------#
+def dump_data(host):
+    '''
+    :param host
+    :return
+    '''
+    result = ""
+    modules = ModuleInfo.query.getHostCoverModules(host)
+    host_suffix = host.split('.')[-1]
+    # ~/RyDeploy/static/report/224-20170818-180754，rpt_folder的目录形式
+    rpt_name = host_suffix + '-' + datatime.datatime.now().strftime('%Y%m%d-%H%M%S')
+    REPORT_BASE_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)),'../static/report')
+    JACOCO_CLI = os.path.join(os.path.abspath(os.path.dirname(__file__)),'../ext','jacococli.jar')
+    rpt_folder = os.path.join(REPORT_BASE_FOLDER, rpt_name)
+    cov_rpt = CovReport(host=host,name = rpt_name)
+    db.session.add(cov_rpt)
+    total_data = {}
+    
+    for module in modules:
+        #dump出来的文件名字
+        #/Users/huihui/RyDeploy/services/../static/report/192.168.1.216-ryoms.exec
+        cov_name = os.path.join(REPORT_BASE_FOLDER,host + "-" + module.module_name + ".exec" )
+        dump_line = 'java -jar {} dump --address {} --port {} --retry 1 --destfile {}'.format(JACOCO_CLI,host,module.cov_port,cov_name)
+        
+
+
+#------------------#

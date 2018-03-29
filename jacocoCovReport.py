dcb1d991821c2633d9f67305480870299ea0c313
huihui19901993 <1451944847@qq.com>
2018-03-29 18:48:09 +0800

    Update jacocoCovReport.py

--- jacocoCovReport.py
+++ jacocoCovReport.py
@@ -58,10 +58,16 @@ class ModuleInfoQuery(BaseQuery):
     def getHostCovModules(self, host):
         result = self.filter_by(host=host,valid=1,cov_monitor=1).all()
         return result
-        
+    def getProjectPathByModule(self,module_name):
+        return self.with_entities(Project.path).filter(Project.id == Modules.project_id).filter(Modules.name == module_name).filter(Modules.valid==1).first()
+    
 #-----------相关数据库操作-------#
 
 #---------4、生成dump文件且产生报告-----------#
+import os
+import subprocess
+import datetime
+from lxml import tree
 #-----------dump_data-------#
 def dump_data(host):
     '''
@@ -82,10 +88,91 @@ def dump_data(host):
     
     for module in modules:
         #dump出来的文件名字
+        #---dump---#
         #/Users/huihui/RyDeploy/services/../static/report/192.168.1.216-ryoms.exec
         cov_name = os.path.join(REPORT_BASE_FOLDER,host + "-" + module.module_name + ".exec" )
         dump_line = 'java -jar {} dump --address {} --port {} --retry 1 --destfile {}'.format(JACOCO_CLI,host,module.cov_port,cov_name)
+        print 'dump_line','-'*10,dump_line
+        p = subprocess.Popen(dump_line, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (stdoutdata, stderrdata) = p.communicate()
+        if(stderrdata.find('Connection refused')>=0):
+          result += "Can not connect to the module {} on {},check the catalina.sh file.".format(module.module_name, host)
+          continue
+          
+          
+        #----report----#
+        project_name = Modules.query.getProjectPathByModule(module.module_name)[0]
+        module_rpt_folder = os.path.join(rpt_folder, module.module_name)     
+        # /home/rongyi/.jenkins/workspace/BUILD-easy-trade-162/easy-trade/easy-rpb/src/main/java
+        # /home/rongyi/.jenkins/workspace/BUILD-easy-trade-162/easy-trade/easy-rpb/target/easy-rpb-5.7.2-SNAPSHOT/WEB-INF/classes
+        # /home/rongyi/.jenkins/workspace/BUILD-easy-trade-162/easy-trade/easy-rpb/src/main/java
+        # /home/rongyi/.jenkins/workspace/BUILD-trade-center-224/trade-checkout/build/classes/main
+        # /home/rongyi/.jenkins/workspace/BUILD-trade-center-224/trade-checkout/src/main/java
+        # easy-trade项目代码层级不一样，需要单独处理
         
+        tmp_project_name = ""
+        if(project_name == "easy-trade" or project_name == "easy-market"):
+            tmp_project_name = project_name
+        src_folder = os.path.join(JENKINS-WORKSPACE, 'BUILD-{}-{}'.format(project_name, host_suffix),tmp_project_name,
+                                  module.module_name,"src/main/java")
+        #gradle编译的class文件要特殊处理
+        if(project_name == "trade-center"):
+            cls_folder = os.path.join(JENKINS_WORKSPACE,'BUILD-{}-{}'.format(project_name,host_suffix),tmp_project_name,
+                                      module.module_name,"build/classes/main")
+        else:
+            cls_folder = os.path.join(JENKINS_WORKSPACE,'BUILD-{}-{}'.format(project_name,host_suffix),tmp_project_name,
+                                     module.module_name,"target",module.module_name + "*","WEB-INF/classes")
+        
+        report_line = 'java -jar {} report {} --sourcefiles {} --classfiles {} --html {}'.format(JACOCO_CLI, cov_name,
+                                     src_folder, cls_folder, module_rpt_folder)
+        print 'report_line','-'*10,report_line
+        
+        p = subprocess.Popen(report_line, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (stdoutdata,stderrdata) = p.communicate()
+        print stderrdata
+        if(stderrdata.find('No such file or directory') >= 0):
+            result += 'Can not find source or class of the module {}.'.format(module.module_name)
+            continue
+            
+        #------解析原始报告-------#
+        
+     #######################################   
+        
+        
+ #------5、解析HTML报告------------#
+from lxml import etree
+def get_ratio(part,total):
+  '''
+  计算覆盖率的比例，返回百分比格式
+  :param part:
+  :param total:
+  :return:
+  '''
+  if(total == 0):
+    return "0"
+  else:
+    return "{:.1%}".format(float(part)/total)
 
-
-#------------------#
+def parse_report_cov(file_name):
+  '''
+  解析默认的报告，获取分支，行等覆盖率信息
+  ：param file_name：原始报告的名字
+  ：return：
+  '''
+  result = {}
+  with open(file_name,'r') as f:
+      total = etree.HTML(f.read())[1].xpath('//tfoot/tr/td')
+      branch = total[3].text.replace(',','')
+      result['branch_all'] = int(branch.split('of')[1])
+      result['branch_cov'] = result['branch_all'] - int(branch.split('of')[0])
+      result['line_all'] = int(total[8].text.replace(',',''))
+      result['line_cov'] = result['line_all'] - int(total[7].text.replace(',',''))
+      result['function_all'] = int(total[10].text.replace(',',''))
+      result['function_cov'] = redult['function_all'] - int(total[9].text.replace(',',''))
+      result['branch_ratio'] = get_ratio(result['branch_cov'],result['branch_all'])
+      result['line_ratio'] = get_ratio(result['line_cov'],result['line_all'])
+      result['function_ratio'] = get_ratio(result['function_cov'],result['function_all'])
+      return result
+#---------------------------------#
+        
+        
